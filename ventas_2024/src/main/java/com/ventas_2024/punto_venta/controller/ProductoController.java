99c5decdbb2100561f358c193e538b7243007903
BillAndyQ <billandyqm@gmail.com>
2024-12-21 09:53:12 -0500

    last_config

--- ventas_2024/src/main/java/com/ventas_2024/punto_venta/controller/ProductoController.java
+++ ventas_2024/src/main/java/com/ventas_2024/punto_venta/controller/ProductoController.java
@@ -17,6 +17,11 @@ public class ProductoController {
     public List<Producto> getProductos() {
         return productoServ.getProductos();
     }
+    
+     @GetMapping("/search/{name}")
+    public List<Producto> searchProductos(@PathVariable String name) {
+        return productoServ.searchProducto(name);  // Pasamos 'name' en lugar de 'producto'
+    }
 
     @GetMapping("/{id}")
     public Producto findProducto(@PathVariable Long id) {
